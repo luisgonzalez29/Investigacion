ecd98fdc8370870110403ae38cf1570212d0a0d2
lgonzalez <lgonzalez@hypernovalabs.com>
2020-05-23 00:31:13 -0500

    Update Home.js

--- src/container/Home.js
+++ src/container/Home.js
@@ -1,30 +1,79 @@
-import React from 'react';
+import React from "react";
 import {
   SafeAreaView,
   StyleSheet,
+  TouchableOpacity,
   ScrollView,
   View,
   Text,
   StatusBar,
-} from 'react-native';
-
+} from "react-native";
+import { Header, Image } from "native-base";
 
 class Home extends React.PureComponent {
-  render (){
-    return(
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-         >
-           
-        </ScrollView>
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Header style={styles.header}>
+          <View style={styles.left}>
+            {/* <Image/> */}
+            </View>
+          <View style={styles.middle}>
+            <Text style={styles.header_title}>Listado de Usuarios</Text>
+            </View>
+          <View style={styles.right}>
+          {/* <Image/> */}
+          </View>
+        </Header>
+        <ScrollView contentInsetAdjustmentBehavior="automatic" style={styles.body}>
+          
+            <TouchableOpacity>
+              <View style={{width:"90%", height:150,backgroundColor:"#dee4e4", marginVertical:10, alignSelf:"center"}}>
+
+              </View>
+            </TouchableOpacity>
+          
+          </ScrollView>
       </SafeAreaView>
-    )
+    );
   }
-};
+}
 
 const styles = StyleSheet.create({
- 
+  container:{
+    width:"100%",
+    backgroundColor:"#f4f6f6"
+  },
+  header:{
+    width:"100%",
+    alignSelf:"center",
+    backgroundColor:"#f4f6f6",
+    borderBottomWidth:0
+  },
+  header_title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    alignSelf:"center",
+    height:"100%"
+  },
+  left:{
+    width:"20%",
+    height:"100%",
+    justifyContent:"center"
+  },
+  middle:{
+    width:"60%",
+    height:"100%",
+    justifyContent:"center"
+  },
+  right:{
+    width:"20%",
+    height:"100%",
+    justifyContent:"center"
+  },
+  body:{
+    backgroundColor:"white"
+  }
 });
 
 export default Home;
