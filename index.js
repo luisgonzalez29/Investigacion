39ae44b659880f10f9de4bebad108a350771ff60
lgonzalez <lgonzalez@hypernovalabs.com>
2020-05-23 15:04:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 //import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
-import Home from './src/container/Home';
+import Forum from './src/container/Forum';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Home);
+AppRegistry.registerComponent(appName, () => Forum);
