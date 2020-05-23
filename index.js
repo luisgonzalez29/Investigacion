76abd43fbe72c8cea5477e2887e603c602c3af4e
lgonzalez <lgonzalez@hypernovalabs.com>
2020-05-23 14:57:19 -0500

    dummy data

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 //import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
-import Profile from '../Investigacion/src/container/Profile';
+import Home from './src/container/Home';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Profile);
+AppRegistry.registerComponent(appName, () => Home);
